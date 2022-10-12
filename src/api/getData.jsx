479fc8cfc3e3647088b0a96ae7dd5c0f5b065935
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-10-12 21:07:46 +0300

    1.7 (try + catch)

--- src/api/getData.jsx
+++ src/api/getData.jsx
@@ -1,12 +1,15 @@
 const getData = async (name, page) => {
-  const data = await fetch(
-    `https://pixabay.com/api/?q=${name}&page=1&key=15486639-fb36d9e164edb7a2c5eb45855&image_type=photo&orientation=horizontal&per_page=${
-      page * 12
-    }`
-  );
-  const parsedData = await data.json();
-  console.log('api', parsedData);
-  return parsedData.hits;
+  try {
+    const data = await fetch(
+      `https://pixabay.com/api/?q=${name}&page=1&key=15486639-fb36d9e164edb7a2c5eb45855&image_type=photo&orientation=horizontal&per_page=${
+        page * 12
+      }`
+    );
+    const parsedData = await data.json();
+    return parsedData.hits;
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export default getData;
